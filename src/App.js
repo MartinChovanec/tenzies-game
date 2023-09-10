55c7ba9ee1454d6b36441bab7cfdc108435a2a3f
Martin Chovanec <chovanec.m11@gmail.com>
2023-09-10 22:38:45 +0200

    basic styling and generate numbers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Die from "./components/Die"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Here will be a tenzies game
-        </p>
-      </header>
-    </div>
-  );
+
+export default function App() {
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.floor((Math.random() * 6) + 1))
+        }
+        return (newDice)
+    }
+    console.log(allNewDice())
+
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
 }
-
-export default App;
