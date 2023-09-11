1606bebf9655047c4b32bbf2749ba4a0b9ff063e
Martin Chovanec <chovanec.m11@gmail.com>
2023-09-11 02:17:16 +0200

    refactoring - backgroundColor for a Die component

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,10 @@
 import React from "react"
 
 export default function Die(props) {
-    console.log(props)
+    const styles = {backgroundColor: props.isHeld? "#59E391" : "white"}
+    
     return (
-        <div className="die-face" style={{backgroundColor: props.isHeld? "#59E391" : "white"}}>
+        <div className="die-face" style={styles}>
             <h2 className="die-num" >{props.value}</h2>
         </div>
     )
