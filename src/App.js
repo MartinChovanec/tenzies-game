5c3c7b4c27169b31327c01c0f7285a5a156244ac
Martin Chovanec <chovanec.m11@gmail.com>
2023-09-11 03:31:44 +0200

    files formatted, rollDice function updated

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
-import React from "react"
-import Die from "./components/Die"
-import { nanoid } from "nanoid"
-
+import React from "react";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
-
-  const [dice, setDice] = React.useState(newAllDice)
+    const [dice, setDice] = React.useState(newAllDice);
 
     function newAllDice() {
-        const newDice = []
+        const newDice = [];
         for (let i = 0; i < 10; i++) {
-          const randomNumber = Math.floor((Math.random() * 6) + 1)
-          newDice.push({id: nanoid(), value: randomNumber, isHeld:false})
+            newDice.push({ id: nanoid(), value: Math.floor(Math.random() * 6 + 1), isHeld: false });
         }
-        return (newDice)
+        return newDice;
     }
 
     function rollDice() {
-      setDice(newAllDice ())
+        setDice((prevDice) =>
+            prevDice.map((die) => {
+                return die.isHeld ? die : { ...die, value: Math.floor(Math.random() * 6 + 1) };
+            })
+        );
     }
 
     function holdDice(id) {
-      setDice(prevDice => prevDice.map(die => {
-        return die.id === id ? 
-            {...die, isHeld: !die.isHeld} :
-            die
-    }))
-  }
-    
-    const allDiceElements = dice.map(die => {
-      return (
-          <Die
-              key = {die.id}
-              value = {die.value}
-              isHeld = {die.isHeld}
-              holdDice={() => holdDice(die.id)}         
-          />
-      )
-  })
+        setDice((prevDice) =>
+            prevDice.map((die) => {
+                return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+            })
+        );
+    }
+
+    const allDiceElements = dice.map((die) => {
+        return <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />;
+    });
 
     return (
         <main>
-            <h1 style={{color:"red"}}>Tenzies game - The project is not finished yet!</h1>
-            <div className="dice-container">
-                {allDiceElements}
-            </div>
+            <h1 style={{ color: "red" }}>Tenzies game - The project is not finished yet!</h1>
+            <div className="dice-container">{allDiceElements}</div>
             <div className="button-roll">
-            <button onClick={rollDice}>Roll</button>
+                <button onClick={rollDice}>Roll</button>
             </div>
         </main>
-    )
+    );
 }
